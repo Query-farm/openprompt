7a3198222fb12bb24902512d7e6d3471829e97e8
Rusty Conover <rusty@conover.me>
2025-12-04 17:19:39 -0500

    fix: update to new extension interface

--- src/include/open_prompt_secret.hpp
+++ src/include/open_prompt_secret.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include "duckdb/main/secret/secret.hpp"
-#include "duckdb/main/extension_util.hpp"
+#include "duckdb/main/extension/extension_loader.hpp"
 
 namespace duckdb {
 
 struct CreateOpenPromptSecretFunctions {
 public:
-    static void Register(DatabaseInstance &instance);
+    static void Register(ExtensionLoader &loader);
 };
 
 } // namespace duckdb
